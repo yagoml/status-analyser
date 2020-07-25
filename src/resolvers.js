a2995e89bc65932741b8e4d717b3c8160a032a0e
yagodml <yagoskor@gmail.com>
2020-07-25 17:45:23 -0300

    daily liquidity added on filter

--- src/resolvers.js
+++ src/resolvers.js
@@ -7,8 +7,8 @@ export default {
     goodStocks: async () => {
       const stocks = await Stock.find()
       return stocks.filter(data => {
-        const {roe, netMargin, dbPl} = data
-        return roe >= 10 && netMargin >= 10 && dbPl <= 0.5
+        const { roe, netMargin, dlPl, dailyLiquidity } = data
+        return roe >= 15 && netMargin >= 15 && dlPl <= 0.5 && dailyLiquidity >= 100000000
       })
     }
   }
